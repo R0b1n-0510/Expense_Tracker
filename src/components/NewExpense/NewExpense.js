31ad82e6e53faea77ee57e0e358ac730938d65e9
R0b1n-0510 <r0b1nofficial@gmail.com>
2023-03-10 12:51:03 +0530

    Chart added and project completed

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,18 @@
+import { useState } from "react"
 import "./NewExpense.css"
 import ExpenseForm from "./ExpenseForm"
 
 const NewExpense = (props) => {
+
+    const [state, setState] = useState(false)
+
+    const startEditingHandler = () => {
+        setState(true)
+    }
+
+    const stopEditingHandler = () => {
+        setState(false)
+    }
 
     const saveExpenseDataHandler = (incomingData) => {
         const expenseData = {
@@ -9,11 +20,13 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData)
+        setState(false)
     }
 
     return <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+        {!state?<button onClick={startEditingHandler}>Add New Expense</button>:""}    
+        {state?<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel = {stopEditingHandler}/>:""}
     </div>
 }
-
+// <div><button onClick={show}>Add Expense</button></div>
 export default NewExpense
